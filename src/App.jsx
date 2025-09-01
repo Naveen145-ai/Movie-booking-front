0272afcf6e8df5f775b4b74ec913731c4bd807b6
Naveen145-ai <naveensrinivas145@gmail.com>
2025-09-01 14:46:02 +0530

    msg2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Movies from './pages/Movies/Movies';
+import Tickets from './pages/Tickets/Tickets';
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
+        <Route path="/tickets" element={<Tickets />} />
       </Routes>
      </Router>
     </>
